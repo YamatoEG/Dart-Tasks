e900aa21dccc130facdee30b6cb98658f5e54ec8
Ahmed Mahmoud <60559798+YamatoEG@users.noreply.github.com>
2025-02-17 20:51:27 +0200

    Update minimunValueOfList.dart

--- Minimun_value/minimunValueOfList.dart
+++ Minimun_value/minimunValueOfList.dart
@@ -2,16 +2,12 @@ void main() {
   List<int> numbers = [54,14,0,67,-1,74,1,44,7,-9];
   print(findMin(numbers));
 }
-//create function that take List of integer numbers
 int findMin(List<int> nums){
-  //I will assign the return varaible with first index of list then check it with the rest of elements
-  int smol=nums[0];
-  
-  //looping on the list and return the min number
+  int minVal=nums[0];
   for(var i =0;i<nums.length;i++){
-    if(smol>nums[i]){
-      smol =nums[i];
+    if(minVal>nums[i]){
+      minVal =nums[i];
     }
   }
-  return smol;
+  return minVal;
 }
